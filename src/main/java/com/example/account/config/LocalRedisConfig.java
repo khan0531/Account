eccada26e3f3bc84bea3dbc95de3a6aa52294ce6
han <rudgksdl94@gmail.com>
2024-01-22 16:38:22 +0900

    스프링 버전 및 그래들 변경

--- src/main/java/com/example/account/config/LocalRedisConfig.java
+++ src/main/java/com/example/account/config/LocalRedisConfig.java
@@ -1,14 +1,14 @@
 package com.example.account.config;
 
-import jakarta.annotation.PostConstruct;
-import jakarta.annotation.PreDestroy;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import redis.embedded.RedisServer;
 
 @Configuration
 public class LocalRedisConfig {
-  @Value("${spring.redis.host}")
+  @Value("${spring.redis.port}")
   private int redisPort;
 
   private RedisServer redisServer;
--- src/main/java/com/example/account/domain/Account.java
+++ src/main/java/com/example/account/domain/Account.java
@@ -1,18 +1,8 @@
 package com.example.account.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import net.bytebuddy.dynamic.loading.InjectionClassLoader.Strategy;
-import org.springframework.stereotype.Service;
+import lombok.*;
+
+import javax.persistence.*;
 
 @Getter
 @Setter
@@ -21,9 +11,8 @@ import org.springframework.stereotype.Service;
 @Builder
 @Entity
 public class Account {
-
   @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @GeneratedValue
   private Long id;
 
   private String accountNumber;
